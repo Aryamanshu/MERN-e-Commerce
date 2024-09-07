499047b3c7be5d68452ffb934edf1a588ba9c856
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-09-07 22:55:17 +0530

    Linked signup and login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+import Home from './pages/Home';
+import LoginPage from "./pages/LoginPage"
 import './App.css';
+import SignUpPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home></Home>),
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage></SignUpPage>,
+  },
+]);
+
+
+  
+
 
 function App() {
   return (
     <div className="App">
-     <Counter />
+     <RouterProvider router={router} />
     </div>
   );
 }
